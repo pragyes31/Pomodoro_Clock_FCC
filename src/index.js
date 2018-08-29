9516a06ac87161d6db84957e87832da73a6530f3
Rahul Nayak <pragyes31@users.noreply.github.com>
2018-08-30 01:53:56 +0530

    new approach

--- src/index.js
+++ src/index.js
@@ -1,92 +1,131 @@
-const playBtn = document.querySelector("#play");
-const timeLeft = document.querySelector(".current-timer");
-const resetBtn = document.querySelector("#reset");
-const sessionLengthNode = document.querySelector(".session");
-const breakLengthNode = document.querySelector(".break");
-const currentTimer = document.querySelector("#current-clock");
-
-function pomodoroClock(sectionId) {
-  const timerLengthNode = document.querySelector(
-    `#${sectionId} .change-length .timer-length`
-  );
-  const plusBtn = document.querySelector(`#${sectionId} .change-length .plus`);
-  const minusBtn = document.querySelector(
-    `#${sectionId} .change-length .minus`
-  );
-  const PomoClock = {
-    addOne: function() {
-      console.log("entered addOne fn");
-      let newLengthValue = parseFloat(timerLengthNode.innerHTML);
-      console.log(typeof newLengthValue);
-      if (newLengthValue >= 60) return;
-      newLengthValue += 1;
-      timerLengthNode.innerHTML = newLengthValue;
-      if (sectionId === "session-length") {
-        timeLeft.innerHTML = `${newLengthValue}:00`;
-      }
+function pomodoroClockFn() {
+  const minusBtns = document.querySelectorAll(".minus");
+  const plusBtns = document.querySelectorAll(".plus");
+  const pomodoroClock = {
+    addOne: function(btnId) {
+      const timerType = btnId === "break-plus" ? "break" : "session";
+      const timerLengthNode = document.querySelector(
+        `#${timerType} .timer-length`
+      );
+      let timerLength = parseFloat(
+        document.querySelector(`#${timerType} .timer-length`).innerHTML
+      );
+      if (timerLength >= 60) return;
+      timerLength += 1;
+      timerLengthNode.innerHTML = timerLength;
     },
-    minusOne: function() {
-      let newLengthValue = parseFloat(timerLengthNode.innerHTML);
-      console.log(typeof newLengthValue);
-      if (newLengthValue <= 1) return;
-      newLengthValue -= 1;
-      timerLengthNode.innerHTML = newLengthValue;
-      if (sectionId === "session-length") {
-        timeLeft.innerHTML = `${newLengthValue}:00`;
-      }
-    },
-    startTimer: function(seconds) {
-      let sessionState = true;
-      let totalSecondsLeft = seconds;
-      let counter = 0;
-      function getMinsSecs() {
-        counter++;
-        let mins = Math.floor(totalSecondsLeft / 60);
-        let secs = Math.floor(totalSecondsLeft % 60);
-        return `${mins}:${secs < 10 ? "0" : ""}${secs}`;
-      }
-      function timerFn() {
-        setTimeout(timerFn(), breakLengthNode.innerHTML * 60);
-        totalSecondsLeft = seconds - counter;
-        timeLeft.innerHTML = getMinsSecs();
-        if (totalSecondsLeft < 1) {
-          clearInterval(intervalFn);
-          // if (sessionState) {
-          //   sessionState = false;
-          //   console.log(sessionState);
-          //   currentTimer.innerHTML = "Break";
-          //   PomoClock.startTimer(breakLengthNode.innerHTML * 60);
-          // } else {
-          //   sessionState = true;
-          //   currentTimer.innerHTML = "session";
-          //   PomoClock.startTimer(sessionLengthNode.innerHTML * 60);
-          // }
-          if ((currentTimer.innerHTML = "Session")) {
-            console.log("reached break");
-            currentTimer.innerHTML = "Break";
-            PomoClock.startTimer(breakLengthNode.innerHTML * 60);
-          } else if ((currentTimer.innerHTML = "Break")) {
-            console.log("reached session");
-            currentTimer.innerHTML = "Session";
-            PomoClock.startTimer(sessionLengthNode.innerHTML * 60);
-          }
-        }
-      }
-      let intervalFn = setInterval(timerFn, 1000);
-    },
-    resetClock: function() {
-      timeLeft.innerHTML = "25:00";
+    subtractOne: function(btnId) {
+      console.log(btnId);
+      const timerType = btnId === "break-minus" ? "break" : "session";
+      console.log(timerType);
+      const timerLength = document.querySelector(`#${timerType} .timer-length`);
+      console.log(typeof timerLength.innerHTML);
+      if (timerLength.innerHTML <= 1) return;
+      timerLength.innerHTML -= 1;
     }
   };
-  plusBtn.addEventListener("click", () => PomoClock.addOne());
-  minusBtn.addEventListener("click", () => PomoClock.minusOne());
-  playBtn.addEventListener("click", () =>
-    PomoClock.startTimer(sessionLengthNode.innerHTML * 60)
+  plusBtns.forEach(plusBtn =>
+    plusBtn.addEventListener("click", e => pomodoroClock.addOne(e.target.id))
   );
-  resetBtn.addEventListener("click", () => PomoClock.resetClock());
-
-  return PomoClock;
+  minusBtns.forEach(minusBtn =>
+    minusBtn.addEventListener("click", e =>
+      pomodoroClock.subtractOne(e.target.id)
+    )
+  );
+  return pomodoroClock;
 }
 
-const breakLengthSection = pomodoroClock("break-length");
-const sessionLengthSection = pomodoroClock("session-length");
+const pomodoroClock = pomodoroClockFn();
+
+// const playBtn = document.querySelector("#play");
+// const timeLeft = document.querySelector(".current-timer");
+// const resetBtn = document.querySelector("#reset");
+// const sessionLengthNode = document.querySelector(".session");
+// const breakLengthNode = document.querySelector(".break");
+// const currentTimer = document.querySelector("#current-clock");
+
+// function pomodoroClock(sectionId) {
+//   const timerLengthNode = document.querySelector(
+//     `#${sectionId} .change-length .timer-length`
+//   );
+//   const plusBtn = document.querySelector(`#${sectionId} .change-length .plus`);
+//   const minusBtn = document.querySelector(
+//     `#${sectionId} .change-length .minus`
+//   );
+//   const PomoClock = {
+//     addOne: function() {
+//       console.log("entered addOne fn");
+//       let newLengthValue = parseFloat(timerLengthNode.innerHTML);
+//       console.log(typeof newLengthValue);
+//       if (newLengthValue >= 60) return;
+//       newLengthValue += 1;
+//       timerLengthNode.innerHTML = newLengthValue;
+//       if (sectionId === "session-length") {
+//         timeLeft.innerHTML = `${newLengthValue}:00`;
+//       }
+//     },
+//     minusOne: function() {
+//       let newLengthValue = parseFloat(timerLengthNode.innerHTML);
+//       console.log(typeof newLengthValue);
+//       if (newLengthValue <= 1) return;
+//       newLengthValue -= 1;
+//       timerLengthNode.innerHTML = newLengthValue;
+//       if (sectionId === "session-length") {
+//         timeLeft.innerHTML = `${newLengthValue}:00`;
+//       }
+//     },
+//     startTimer: function(seconds) {
+//       let sessionState = true;
+//       let totalSecondsLeft = seconds;
+//       let counter = 0;
+//       function getMinsSecs() {
+//         counter++;
+//         let mins = Math.floor(totalSecondsLeft / 60);
+//         let secs = Math.floor(totalSecondsLeft % 60);
+//         return `${mins}:${secs < 10 ? "0" : ""}${secs}`;
+//       }
+//       function timerFn() {
+//         setTimeout(timerFn(), breakLengthNode.innerHTML * 60);
+//         totalSecondsLeft = seconds - counter;
+//         timeLeft.innerHTML = getMinsSecs();
+//         if (totalSecondsLeft < 1) {
+//           clearInterval(intervalFn);
+//           // if (sessionState) {
+//           //   sessionState = false;
+//           //   console.log(sessionState);
+//           //   currentTimer.innerHTML = "Break";
+//           //   PomoClock.startTimer(breakLengthNode.innerHTML * 60);
+//           // } else {
+//           //   sessionState = true;
+//           //   currentTimer.innerHTML = "session";
+//           //   PomoClock.startTimer(sessionLengthNode.innerHTML * 60);
+//           // }
+//           if ((currentTimer.innerHTML = "Session")) {
+//             console.log("reached break");
+//             currentTimer.innerHTML = "Break";
+//             PomoClock.startTimer(breakLengthNode.innerHTML * 60);
+//           } else if ((currentTimer.innerHTML = "Break")) {
+//             console.log("reached session");
+//             currentTimer.innerHTML = "Session";
+//             PomoClock.startTimer(sessionLengthNode.innerHTML * 60);
+//           }
+//         }
+//       }
+//       let intervalFn = setInterval(timerFn, 1000);
+//     },
+//     resetClock: function() {
+//       timeLeft.innerHTML = "25:00";
+//     }
+//   };
+//   plusBtn.addEventListener("click", () => PomoClock.addOne());
+//   minusBtn.addEventListener("click", () => PomoClock.minusOne());
+//   playBtn.addEventListener("click", () =>
+//     PomoClock.startTimer(sessionLengthNode.innerHTML * 60)
+//   );
+//   resetBtn.addEventListener("click", () => PomoClock.resetClock());
+
+//   return PomoClock;
+// }
+
+// const breakLengthSection = pomodoroClock("break-length");
+// const sessionLengthSection = pomodoroClock("session-length");
